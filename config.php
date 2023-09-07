d83c2382c20eaefda0e01866c83c60d6159843be
Munirt98 <126955380+Munirt98@users.noreply.github.com>
2023-09-07 19:02:42 +0300

    adding website files
    adding website files to github respository

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'swiftarabic';
+$CFG->dbuser    = 'swiftarabic';
+$CFG->dbpass    = '$Amman2013';
+$CFG->prefix    = 'sar_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'https://swiftarabic.com';
+$CFG->dataroot  = '/var/www/swiftarabic/data';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
